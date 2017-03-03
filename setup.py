fc8e8352898f8c4cb666510da1babbab5b6dea86
Suraj Arya <suraj.p.arya@gmail.com>
2017-03-03 15:25:36 +0530

    * fixed some issues in setup.py because of which the pypi uploads were failing

--- setup.py
+++ setup.py
@@ -3,22 +3,18 @@
 
 from setuptools import setup
 
-with open('README.md') as readme_file:
-    readme = readme_file.read()
-
-
 requirements = [
     "elasticsearch==5.1.0"
 ]
 
 
 setup(
-    name='elasticstats',
-    version='0.1.0',
+    name='elasticstats-scrapy',
+    version='0.1.1',
     description="A scrapy extension to send crawl stats to elasticsearch index.",
     author="Suraj Arya",
     author_email='suraj@loanzen.in',
-    url='https://github.com/suraj-arya/elasticstats',
+    url='https://github.com/suraj-arya/elasticstats-scrapy',
     packages=[
         'elasticstats',
     ],
